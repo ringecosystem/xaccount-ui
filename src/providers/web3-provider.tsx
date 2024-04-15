a4e444c8805d05c642b4b70ed9c166605987fd7a
snoopy1412 <snoopy1412.su@gamil.com>
2024-04-15 18:24:17 +0800

    Refactor dashboard component and initialize database in web3 provider

--- src/providers/web3-provider.tsx
+++ src/providers/web3-provider.tsx
@@ -3,22 +3,31 @@ import { useAccount } from 'wagmi';
 
 import useChainStore from '@/store/chain';
 import { getDefaultChain } from '@/utils';
+import { initXAccountsDB } from '@/database/xaccounts';
+import { initDappsRepositoryDB } from '@/database/dapps-repository';
 
 type Web3ProviderProps = {
   children: ReactNode;
 };
 
+if (typeof window !== 'undefined') {
+  initDappsRepositoryDB();
+  initXAccountsDB();
+}
+
 export const Web3Provider = ({ children }: Web3ProviderProps) => {
   const { chain } = useAccount();
   const setLocalChain = useChainStore((state) => state.setLocalChain);
+  const removeRemoteChain = useChainStore((state) => state.removeRemoteChain);
 
   useEffect(() => {
     if (chain) {
       setLocalChain(chain);
+      removeRemoteChain();
     } else {
       setLocalChain(getDefaultChain());
     }
-  }, [chain, setLocalChain]);
+  }, [chain, removeRemoteChain, setLocalChain]);
 
   return <>{children}</>;
 };
