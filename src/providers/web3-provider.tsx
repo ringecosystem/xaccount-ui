18411391de06627d94122602e4d2b151c1118244
snoopy1412 <snoopy1412.su@gamil.com>
2024-05-10 20:49:00 +0800

    refactor: Improve Web3Provider component initialization

--- src/providers/web3-provider.tsx
+++ src/providers/web3-provider.tsx
@@ -1,45 +1,53 @@
-import React, { useEffect, ReactNode } from 'react';
-import { useAccount } from 'wagmi';
+import React, { useEffect, ReactNode, useRef } from 'react';
+import { useAccount, useChainId } from 'wagmi';
 
 import useChainStore from '@/store/chain';
 import { initXAccountsDB } from '@/database/xaccounts';
 import { initDappsRepositoryDB } from '@/database/dapps-repository';
 import TransactionManager from '@/components/transaction-manager';
-import { usePrevious } from 'react-use';
 import useReturnDashboard from '@/hooks/useReturnDashboard';
 
 type Web3ProviderProps = {
   children: ReactNode;
 };
 
-if (typeof window !== 'undefined') {
-  initDappsRepositoryDB();
-  initXAccountsDB();
-}
-
+const initDatabases = () => {
+  if (typeof window !== 'undefined') {
+    initDappsRepositoryDB();
+    initXAccountsDB();
+  }
+};
 export const Web3Provider = ({ children }: Web3ProviderProps) => {
-  const { chainId, address } = useAccount();
+  const { address } = useAccount();
+
+  const chainId = useChainId();
 
   const removeRemoteChain = useChainStore((state) => state.removeRemoteChain);
 
   const returnDashboard = useReturnDashboard();
 
-  const previousChainId = usePrevious(chainId);
-  const previousAddress = usePrevious(address);
+  const previousChainId = useRef(chainId);
+  const previousAddress = useRef(address);
 
   useEffect(() => {
-    if (previousChainId && chainId !== previousChainId) {
+    if (previousChainId.current !== chainId) {
       removeRemoteChain();
       returnDashboard();
     }
-  }, [chainId, previousChainId, removeRemoteChain, returnDashboard]);
+    previousChainId.current = chainId;
+  }, [chainId, removeRemoteChain, returnDashboard]);
 
   useEffect(() => {
-    if (previousAddress && address !== previousAddress) {
+    if (previousAddress.current !== address) {
       removeRemoteChain();
       returnDashboard();
     }
-  }, [address, previousAddress, removeRemoteChain, returnDashboard]);
+    previousAddress.current = address;
+  }, [address, removeRemoteChain, returnDashboard]);
+
+  useEffect(() => {
+    initDatabases();
+  }, []);
 
   return (
     <>
