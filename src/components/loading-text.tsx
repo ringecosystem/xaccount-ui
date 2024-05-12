67a7394c4fa91ea5de6688e118cb234badc9f08a
snoopy1412 <snoopy1412.su@gamil.com>
2024-05-12 17:37:10 +0800

    refactor: Update size class in LoadingText component to use rem unit instead of px

--- src/components/loading-text.tsx
+++ src/components/loading-text.tsx
@@ -10,7 +10,7 @@ type LoadingTextProps = {
 };
 
 const LoadingText: React.FC<LoadingTextProps> = ({ isLoading, text, spinClassName }) => {
-  return <>{isLoading ? <Spin className={cn('mr-2 size-3', spinClassName)} /> : text}</>;
+  return <>{isLoading ? <Spin className={cn('mr-2 size-[0.875rem]', spinClassName)} /> : text}</>;
 };
 
 export default LoadingText;
